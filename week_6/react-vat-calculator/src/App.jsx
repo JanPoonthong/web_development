767601b4a8923fa5d7473f04f081c7917c17be28
Jan Poonthong <janpoonthong628@gmail.com>
2024-07-10 12:07:12 +0700

    feat: discount

--- week_6/react-vat-calculator/src/App.jsx
+++ week_6/react-vat-calculator/src/App.jsx
@@ -3,43 +3,60 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [a, setA] = useState("");
+  const [a, setA] = useState(0);
   const [b, setB] = useState(0);
   const [vat, setVat] = useState(0);
 
-  function increaseB(a) {
-    return setB(count + a);
+  function increaseA() {
+    setA(a + 1)
   }
 
-  function onChangeHanlder(event) {
+  function increaseB() {
+    setVat(count + a)
+  }
+
+  function onChangeHanlderPrice(event) {
     const price = event.target.value;
     setA(price);
     setVat(price * 0.07);
   }
 
+  function onChangeHanlderDiscount(event) {
+    const discount = event.target.value;
+    setB(discount)
+    setVat((a - discount) * 0.07);
+  }
+
+
   return (
     <>
       <div className="card">
-        <input
+        <label htmlFor="">Price: </label>
+      <input
           type="number"
           style={{ fontSize: "20px" }}
           value={a}
-          onChange={(event) => onChangeHanlder(event)}
+          placeholder="Price"
+          onChange={(event) => onChangeHanlderPrice(event)}
+        ></input><br /><br />
+        <label htmlFor="">Discount: </label>
+        <input
+          type="number"
+          style={{ fontSize: "20px" }}
+          value={b}
+          placeholder="Discount"
+          onChange={(event) => onChangeHanlderDiscount(event)}
         ></input>
+
         <h1>My A = {a === "" ? "0" : a}</h1>
         <h1>VAT = {Math.round(vat * 100) / 100}</h1>
 
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <button onClick={() => increaseB(2)}>count is {b}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {/* <button onClick={() => increaseA()}>Increase A</button> */}
+        {/* <button onClick={() => increaseB()}>Increase B</button> */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
