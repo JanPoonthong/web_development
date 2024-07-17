5e4dcbaa5fa657042d8bdf2dc23b02571a2657c5
Jan Poonthong <janpoonthong628@gmail.com>
2024-07-17 10:55:19 +0700

    feat: update data table with selected item

--- week_7/accessory_shop/src/App.jsx
+++ week_7/accessory_shop/src/App.jsx
@@ -6,17 +6,27 @@ function App() {
   const quantityRef = useRef();
   const productRef = useRef();
   const [price, setPrice] = useState(accessoryData[0].price);
+  const [selectedItems, setSelectedItems] = useState([
+    { id: 1, name: "Mouse", price: 10, quantity: 2 },
+    { id: 2, name: "Keyboard", price: 20, quantity: 1 },
+  ])
 
   const handleInput = () => {
-    quantityRef.current.value;
-    productRef.current.value;
+      quantityRef.current.value
+      productRef.current.value
   };
 
   const updatePrice = (event) => {
     const productID = parseInt(event.target.value);
     const product = accessoryData.find(
-      (accessory) => accessory.id === productID,
+      (accessory) => accessory.id === productID
     );
+
+    const order = {
+      ...product,
+      quantity: quantityRef.current.value
+    }
+    setSelectedItems(oldState => [...oldState, order])
     setPrice(product.price);
   };
 
@@ -41,7 +51,7 @@ function App() {
       <input type="number" ref={quantityRef}></input>
       <button onClick={handleInput}>Submit</button>
 
-      <DataTable data={accessoryData}></DataTable>
+      <DataTable data={selectedItems}></DataTable>
     </>
   );
 }
